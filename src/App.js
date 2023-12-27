c2653c4e72e237d3927ffd029c0c41fdd931061c
Praveen <devpraveenkr@gmail.com>
2023-12-27 14:19:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, MemoryRouter, HashRouter, Routes, Route } from "react-router-dom";
+import Profile from './components/Profile';
+import ProfileForm from './components/ProfileForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MemoryRouter>
+        <Routes>
+          <Route exact path="/" element={<Profile />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/update" element={<ProfileForm />} />
+        </Routes>
+      </MemoryRouter>
     </div>
   );
 }
